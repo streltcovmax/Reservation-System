197e6be53d44ed3b0681f2019d41b1770fdd47c9
streltcovmax <144123001+streltcovmax@users.noreply.github.com>
2025-01-08 21:59:52 +0300

    Delete demo/src/main/main directory

--- demo/src/main/main/java/com/example/demo/repositories/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.model.AdminData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AdminRepository extends MongoRepository<AdminData, String> {
-    AdminData findByName(String name);
-}
